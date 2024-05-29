941dc9b0281a3f5af555b9eb8e64e6ebb2392b60
Allan <allan.mendes0976@gmail.com>
2024-05-29 11:19:27 -0300

    jogo de adivinhar

---
+++ index.js
@@ -0,0 +1,81 @@
+let aleatorio = Math.floor(Math.random() * 100 + 1);
+let palpite = document.getElementById('palpite');
+const button = document.getElementById('button');
+const restart = document.getElementById('restart');
+const resposta = document.querySelector('.resposta');
+const acertou = document.querySelector('.acertou');
+const errou = document.querySelector('.errou');
+const contador = [];
+
+console.log(aleatorio, palpite.value);
+const evento = button.addEventListener('click', function () {
+    let valorPalpite = parseInt(palpite.value);
+    if(aleatorio === valorPalpite) {
+        acertouMiseravy();
+        palpite.focus();
+        palpite.value = '';
+        
+        button.style.display = 'none';
+        restart.style.display = 'block';
+    }
+    if (aleatorio > valorPalpite) {
+        contador.length === 9 ? gameOver() : contador.push(valorPalpite);
+        palpite.focus();
+        palpite.value = ''
+
+        resposta.style.display = 'block';
+
+        resposta.textContent = `Tente um número mais alto. Números jogados: ${contador}`;
+        resposta.classList.add('tentativas');
+    }
+    
+    if (aleatorio < valorPalpite) {
+        contador.length === 9 ? gameOver() : contador.push(valorPalpite);
+        palpite.focus();
+        palpite.value = ''
+
+        resposta.textContent = `Tente um número mais baixo.<br> Números jogados: ${contador}`;
+        resposta.classList.add('tentativas');
+
+    }
+});
+
+function acertouMiseravy() {
+    palpite.focus();
+    palpite.value = '';
+    acertou.style.display = 'block';
+    acertou.textContent = `Acertou essa bagaça!`;
+}
+
+
+function gameOver() {
+    palpite.focus();
+    palpite.value = '';
+    button.style.display = 'none';
+    restart.style.display = 'block';
+    errou.textContent = `Errooouu!`;
+    errou.style.display = 'block';
+}
+
+const jogarNovamente = restart.addEventListener('click', function () {
+    if (document.body.querySelector('.errou') || 
+        document.body.querySelector('.acertou') || 
+        document.body.querySelector('.tentativas')) {
+        aleatorio = Math.floor(Math.random() * 100 + 1);
+        document.querySelectorAll('.acertou, .errou, .tentativas').forEach(element => {
+            palpite.focus();
+            palpite.value = '';
+            contador.length = 0;
+            element.style.display = 'none';
+            button.style.display = 'block';
+            restart.style.display = 'none';
+        });
+    }
+});
+
+ /* 
+- Tentar fazer com que a div de resposta não altere a posição dos outros elementos 
+- Achar alguma cor que combine para o fundo
+- Tentar cirar um lugar separado para armazenar os números já enviados 
+- Implementar um botão de dicas
+*/
